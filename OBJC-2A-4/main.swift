af34d125b46d7b3d2cbb1fb2cfe237adefe56e54
Magnus Kruschwitz <magnus.kruschwitz@outlook.com>
2018-07-01 21:28:18 +0200

    Initial Commit

---
+++ OBJC-2A-4/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  OBJC-2A-4
+//
+//  Created by Magnus Kruschwitz on 01.07.18.
+//  Copyright © 2018 Magnus Kruschwitz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
