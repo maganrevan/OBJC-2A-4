0bfba399d49c59e549c4fb6861d42eb31448f5c2
Magnus Kruschwitz <magnus.kruschwitz@outlook.com>
2018-07-01 21:45:57 +0200

    Aufgabe 4. OBJC 2A-XX1-N01

--- OBJC-2A-4/main.swift
+++ OBJC-2A-4/main.swift
@@ -1,3 +1,5 @@
+/* ################################################ Einsendeaufgabe 2.4
+   ################################################# */
 //
 //  main.swift
 //  OBJC-2A-4
@@ -7,6 +9,26 @@
 //
 
 import Foundation
+/* Ausgangssituattion :
+var varInt = 10
+print("Ein Zentimeter entspricht \(varInt) Millimetern.")
+varInt = 10 * 100
+print("Ein Meter entspricht \(varInt) Millimetern.")
+*/
 
-print("Hello, World!")
+/* Möglichkeit 1:
+
+let cBase : Int = 10
+let cFactor : Int = 100
+print("Ein Zentimeter entspricht \(cBase) Millimetern.")
+print("Ein Meter entspricht \(cBase * cFactor) Millimetern.")
+ */
+
+/* Möglichkeit 2:
+ */
+let cMilliZenti : Int = 10
+//let cMilliMet : Int = 1000
+let cMilliMet : Int = cMilliZenti * 100
+print("Ein Zentimeter entspricht \(cMilliZenti) Millimetern.")
+print("Ein Meter entspricht \(cMilliMet) Millimetern.")
 
